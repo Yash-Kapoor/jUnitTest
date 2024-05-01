697a2c475eef20091c4184a2fe212d7c4cb7e20b
yashk <yashk@10.12.79.109>
2024-05-01 09:06:18 +0530

    Commit

--- src/jUnitTestPackage/jUnitFunctions.java
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -1,5 +1,20 @@
 package jUnitTestPackage;
 
-public class jUnitFunctions {
+import java.util.Scanner;
 
+public class jUnitFunctions {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        double firstNumber = scanner.nextDouble();
+        
+        System.out.print("Enter the second number: ");
+        double secondNumber = scanner.nextDouble();
+        
+        double sum = firstNumber + secondNumber;
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is " + sum);
+        
+        scanner.close();
+    }
 }
